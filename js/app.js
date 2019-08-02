903b043eee31827a479ca911e095758d16e78dac
Fidelma <fidelma.beagan@gmail.com>
2019-08-02 18:16:43 +0100

    radio buttons not working yet

--- js/app.js
+++ js/app.js
@@ -1,6 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
   const form = document.querySelector('#new-item-form');
-  form.addEventListener('submit', handleNewFormSubmit);
+  form.addEventListener('submit',  handleNewFormSubmit);
+
+  // const contract = document.querySelector('.contract-options');
+  // contract.addEventListener('click', () => {
+  // });
 
   const deleteAll = document.querySelector('button');
   deleteAll.addEventListener('click', handleDeleteAll);
@@ -13,8 +17,10 @@ const handleNewFormSubmit = function (event) {
   const members = document.querySelector('#members-list');
   members.appendChild(newMember);
 
+
   event.target.reset();
 }
+
 
 const createNewMember = function(form) {
   const newMember = document.createElement('li');
@@ -32,8 +38,29 @@ const createNewMember = function(form) {
   flat.textContent = event.target.flat.value;
   newMember.appendChild(flat);
 
+  const room = document.createElement('h4');
+  room.textContent = event.target['room-options'].value;
+  newMember.appendChild(room);
+
   const contract = document.createElement('h4');
-  contract.textContent = event.target['contract-types'].value;
+  console.dir(event);
+    const oneYear = document.querySelector('#contract-1-year');
+    const nineMonths = document.querySelector('#contract-9-months');
+    const sixMonths = document.querySelector('#contract-6-months');
+
+    const handler = () => {
+      console.log('clicked');
+    }
+    
+    oneYear.onclick = handler;
+    nineMonths.onclick = handler;
+    sixMonths.onclick = handler;
+
+  // const contractType = document.querySelector('.contract-options');
+  // contractType.addEventListener('click', (event) => {
+  //   contract.textContent = event.target.value;
+  //   // const actualContractType = event.target.input.value;
+  // })
   newMember.appendChild(contract);
 
   return newMember;
